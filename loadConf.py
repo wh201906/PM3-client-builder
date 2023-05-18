a1921feeda0d432bdce7abf99c6486bcf3d22c0f
wh201906 <wh201906@yandex.com>
2023-05-18 15:02:13 +0800

    Fix checkout

--- loadConf.py
+++ loadConf.py
@@ -6,5 +6,5 @@ conf = json.load(confFile)
 confFile.close()
 
 with open(environ["GITHUB_OUTPUT"], "a") as f:
-    # the keys there matches the keys in jobs->load_conf->outputs of build.yml 
+    # the keys there matches the keys in jobs->load_conf->outputs of build.yml
     f.write("refs=" + str(conf["refs"]) + "\n")
