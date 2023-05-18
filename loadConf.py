be94fdb753c753bebedcfb35006503429cbd0c9c
wh201906 <wh201906@yandex.com>
2023-05-18 21:46:32 +0800

    configurable platforms

--- loadConf.py
+++ loadConf.py
@@ -8,3 +8,4 @@ confFile.close()
 with open(environ["GITHUB_OUTPUT"], "a") as f:
     # the keys there matches the keys in jobs->load_conf->outputs of build.yml
     f.write("refs=" + str(conf["refs"]) + "\n")
+    f.write("platforms=" + str(conf["platforms"]) + "\n")
